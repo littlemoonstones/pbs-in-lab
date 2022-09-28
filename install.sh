295f6227af71a19e9589f59a7c6d6b003cf1892d
littlemoonstones <littlemoonstones@gmail.com>
2022-09-28 09:02:19 +0800

    improve install process

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 mkdir -p ~/bin
-chmod +x pbs_lib/* && cp pbs_lib/* ~/bin/
-echo "export PATH=\"${HOME}/bin/:\$PATH\"" >> ~/.bashrc
+
+# Check if PATH is installed:
+if ! grep "PATH=\"$HOME/bin/" ~/.bashrc > /dev/null; then
+    echo "export PATH=\"${HOME}/bin/:\$PATH\"" >> ~/.bashrc
+    echo "add path..."
+else
+    echo "path already existed"
+fi
+
+echo "copy files to $HOME/bin/"
+if chmod +x pbs_lib/* && cp pbs_lib/* ~/bin/; then
+    echo "Done"
+fi
